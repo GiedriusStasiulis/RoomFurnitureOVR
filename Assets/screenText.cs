f154ded0df9fe2101fc8af57f949cd349b826744
niuxe <kristian.d.kristensen@gmail.com>
2020-11-22 19:06:29 +0100

    Added script and text box to the game

---
+++ Assets/screenText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class screenText : MonoBehaviour
+{
+    public Text mytext = null;
+    private int counter = 0;
+
+    public void nextStep()
+    {
+        counter++;
+        mytext.text = "Test " + counter;
+    }
+
+    public void prevStep()
+    {
+        counter--;
+        mytext.text = "Test " + counter;
+    }
+
+}
